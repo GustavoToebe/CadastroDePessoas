ede6d51429d700982e75600124184cd4df717375
GustavoToebe <gustavotoebe4@gmail.com>
2024-12-16 19:27:10 -0300

    Começando o projeto

---
+++ src/main/java/dev/java10x/cadastrodepessoas/CadastroDePessoasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastrodepessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePessoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDePessoasApplication.class, args);
+    }
+
+}
